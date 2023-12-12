016b44b428ac51b56b05bca7bce75d3e8636b74c
Berychc <treewincsgo@gmail.com>
2023-12-12 16:55:47 +0300

    Done

--- src/main/java/com/collections/Exceptions/EmployeeAlreadyAddedException.java
+++ src/main/java/com/collections/Exceptions/EmployeeAlreadyAddedException.java
@@ -2,6 +2,6 @@ package com.collections.Exceptions;
 
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException() {
-        super("Коллекции уже есть такой сотрудник");
+        super("В коллекции уже есть такой сотрудник");
     }
 }
